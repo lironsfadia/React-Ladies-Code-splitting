12f9e625ae3396e6ae310fbe15d966ccd2470626
Liron <liron.ssss@gmail.com>
2022-05-10 17:01:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+// import Card from "./card";
+import React, { Suspense } from "react";
+import Spinner from "./Spinner";
+import Error from "./Error";
+const Card = React.lazy(() => import("./Card"));
+// const Card = React.lazy(() => Promise.reject());
+
+function App() {
+  return (
+    <div className="container">
+      <Error fallback={<p>card failed to load</p>}>
+        <header className="display-1">React Ladies Pro Edition | 2022</header>
+      </Error>
+
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+      <Error fallback={<p>card failed to load</p>}>
+        <Suspense fallback={<Spinner />}>
+          <Card />
+        </Suspense>
+      </Error>
+    </div>
+  );
+}
+
+export default App;
